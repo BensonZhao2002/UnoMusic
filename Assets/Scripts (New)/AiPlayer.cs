93b46ad6ca6d8b180b33bb10e997d9e4c6e46d52
BensonZhao2002 <hzhao355@gatech.edu>
2024-11-20 15:16:24 -0500

    UnoMusic_GameSetUp

---
+++ Assets/Scripts (New)/AiPlayer.cs
@@ -0,0 +1,226 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class AiPlayer : MonoBehaviour, PlayerInterface {
+
+	bool skip=false;
+	bool drew =false;
+	string name;
+	string colorToPlay;
+	int locationCardPlayed=-1;
+	List<Card> handList = new List<Card> ();
+
+	public AiPlayer(string name) { //initializes
+		this.name = name;
+	}
+
+	public bool skipStatus {
+		get{return skip; }
+		set{ skip = value; }
+	}
+
+	public void turn() { //the ai's turn
+		Control control = GameObject.Find("Control").GetComponent<Control>();
+		control.StartCoroutine(AITurnRoutine());
+		/*
+		Card currDisc = Control.discard [Control.discard.Count - 1];
+		string colorDisc = currDisc.getColor ();
+		int numbDisc = currDisc.getNumb ();
+		if (numbDisc == 13 || numbDisc == 14)
+			numbDisc = -1;
+
+		locationCardPlayed = -1;
+		colorToPlay = "";
+		drew = false;
+
+		handList = handList.OrderBy (e => e.getColor()).ThenBy(e => e.getNumb ()).ToList();
+		
+		int count = handList.Count (e => e.getColor () == colorDisc); //does counts of how many cards it has, either color, number, or wild
+		int count2 = handList.Count (e => e.getNumb () == numbDisc);
+		int count3 = handList.Count (e => e.getNumb () == 13 || e.getNumb () == 14);
+
+		if (count > 0) 
+			locationCardPlayed = handList.FindLastIndex (e => e.getColor () == colorDisc);
+		
+		else if (count2 > 0) 
+			locationCardPlayed = handList.FindIndex (e => e.getNumb () == numbDisc);
+
+		else if (count3 > 0) {
+			locationCardPlayed = handList.FindIndex (e => e.getNumb () == 13 || e.getNumb()==14);
+			colorToPlay = handList.Max (e => e.getColor ());
+			if (colorToPlay.Equals ("Black")) {
+				bool first = true;
+				while(colorToPlay.Equals(colorDisc) || first) {
+					int rand = Random.Range (1, 4);
+					switch (rand) {
+						case 1:
+							colorToPlay = "Red";
+							break;
+						case 2:
+							colorToPlay = "Blue";
+							break;
+						case 3:
+							colorToPlay = "Green";
+							break;
+						case 4:
+							colorToPlay = "Yellow";
+							break;
+					}
+					first = false;
+				}
+			}
+					
+		}
+		else {
+			GameObject.Find ("Control").GetComponent<Control> ().draw (1, this);
+			drew = true;
+		}
+
+		if (locationCardPlayed == -1 && !drew) 
+			return;
+		
+		else 
+			turnEnd ();
+		*/
+	}
+
+	private IEnumerator AITurnRoutine()
+	{
+		// Wait for the configured AI wait time
+		float aiWaitTime = GameObject.Find("Control").GetComponent<Control>().aiWaitTime;
+		yield return new WaitForSeconds(aiWaitTime);
+
+		// Execute the AI's turn logic
+		Card currDisc = Control.discard[Control.discard.Count - 1];
+		string colorDisc = currDisc.getColor();
+		int numbDisc = currDisc.getNumb();
+		if (numbDisc == 13 || numbDisc == 14)
+			numbDisc = -1;
+
+		locationCardPlayed = -1;
+		colorToPlay = "";
+		drew = false;
+
+		handList = handList.OrderBy(e => e.getColor()).ThenBy(e => e.getNumb()).ToList();
+
+		int count = handList.Count(e => e.getColor() == colorDisc);
+		int count2 = handList.Count(e => e.getNumb() == numbDisc);
+		int count3 = handList.Count(e => e.getNumb() == 13 || e.getNumb() == 14);
+
+		if (count > 0)
+			locationCardPlayed = handList.FindLastIndex(e => e.getColor() == colorDisc);
+		else if (count2 > 0)
+			locationCardPlayed = handList.FindIndex(e => e.getNumb() == numbDisc);
+		else if (count3 > 0)
+		{
+			locationCardPlayed = handList.FindIndex(e => e.getNumb() == 13 || e.getNumb() == 14);
+			colorToPlay = handList.Max(e => e.getColor());
+			if (colorToPlay.Equals("Black"))
+			{
+				bool first = true;
+				while (colorToPlay.Equals(colorDisc) || first)
+				{
+					int rand = Random.Range(1, 4);
+					switch (rand)
+					{
+						case 1: colorToPlay = "Red"; break;
+						case 2: colorToPlay = "Blue"; break;
+						case 3: colorToPlay = "Green"; break;
+						case 4: colorToPlay = "Yellow"; break;
+					}
+					first = false;
+				}
+			}
+		}
+		else
+		{
+			GameObject.Find("Control").GetComponent<Control>().draw(1, this);
+			drew = true;
+		}
+
+		if (locationCardPlayed == -1 && !drew)
+			yield break; // End coroutine if no valid action is found
+
+		turnEnd(); // Complete the turn
+	}
+
+	public void addCards(Card other) { //recieves cards to the hand
+		handList.Add (other);
+	}
+
+	public void turnEnd()
+	{
+		Control cont = GameObject.Find("Control").GetComponent<Control>();
+
+		if (drew)
+		{
+			cont.recieveText($"{name} drew");
+			cont.enabled = true;
+			return;
+		}
+
+		// Save card data before modifying the hand list
+		Card playedCard = handList[locationCardPlayed];
+		int toneIndex = playedCard.getNumb();
+		string color = playedCard.getColor();
+
+		// Handle special card effects and update discard pile
+		int specNumb = playedCard.getNumb();
+		if (specNumb < 10)
+		{
+			cont.recieveText($"{name} played a {playedCard.getColor()} {playedCard.getNumb()}");
+			cont.enabled = true;
+		}
+		else if (specNumb == 10)
+		{
+			cont.specialCardPlay(this, 10);
+			cont.recieveText($"{name} played a {playedCard.getColor()} skip");
+		}
+		else if (specNumb == 11)
+		{
+			cont.specialCardPlay(this, 11);
+			cont.recieveText($"{name} played a {playedCard.getColor()} reverse");
+		}
+		else if (specNumb == 12)
+		{
+			cont.specialCardPlay(this, 12);
+			cont.recieveText($"{name} played a {playedCard.getColor()} draw 2");
+		}
+		else if (specNumb == 13)
+		{
+			cont.recieveText($"{name} played a wild card, Color: {colorToPlay}");
+			playedCard.changeColor(colorToPlay);
+			cont.enabled = true;
+		}
+		else if (specNumb == 14)
+		{
+			cont.specialCardPlay(this, 14);
+			cont.recieveText($"{name} played a wild draw 4, Color: {colorToPlay}");
+			playedCard.changeColor(colorToPlay);
+			cont.enabled = true;
+		}
+
+		// Play card audio using saved data
+		if (toneIndex >= 0 && toneIndex < 10)
+		{
+			cont.PlayCardAudio(color, toneIndex);
+		}
+
+		// Update discard pile and remove the card
+		cont.updateDiscPile(playedCard);
+		handList.RemoveAt(locationCardPlayed);
+	}
+
+	public bool Equals(PlayerInterface other) { //equals
+		return other.getName ().Equals (name);
+	}
+	public string getName() { //returns the name
+		return name;
+	}
+	public int getCardsLeft() { //returns cards left
+		return handList.Count;
+	}
+}
---
+++ Assets/Scripts (New)/HumanPlayer.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HumanPlayer : MonoBehaviour, PlayerInterface {
+
+	bool skip=false;
+	bool drew =false;
+	bool playedWild;
+	string name;
+	List<Card> handList = new List<Card> ();
+
+	public HumanPlayer(string name) { //initalizes
+		this.name = name;
+	}
+
+	public bool skipStatus { //returns if the player should be skipped
+		get{return skip; }
+		set{ skip = value; }
+	}
+
+	public void turn() { //does the turn
+		playedWild = false;
+		drew = false;
+		int i = 0;
+		foreach (Card x in handList) { //foreach card in hand
+			
+			GameObject temp = null;
+			if (GameObject.Find ("Control").GetComponent<Control> ().playerHand.transform.childCount > i) //is the card already there or does it need to be loaded
+				temp = GameObject.Find ("Control").GetComponent<Control> ().playerHand.transform.GetChild (i).gameObject;			
+			else 
+				temp = x.loadCard (GameObject.Find ("Control").GetComponent<Control> ().playerHand.transform);
+
+			
+			if (handList [i].Equals (Control.discard [Control.discard.Count - 1]) || handList [i].getNumb () >= 13) { //if the cards can be played
+				setListeners (i, temp);
+			}
+			else {
+				temp.transform.GetChild (3).gameObject.SetActive (true); //otherwise black them out
+			}
+			i++;
+		}
+	}
+	public void setListeners(int where,GameObject temp) { //sets all listeners on the cards
+		temp.GetComponent<Button> ().onClick.AddListener (() => {
+			playedWild = handList[where].getNumb()>=13;
+
+			temp.GetComponent<Button>().onClick.RemoveAllListeners();
+			Destroy (temp);
+			turnEnd(where);
+		});
+	}
+	public void addCards(Card other) { //recieves cards to add to the hand
+		handList.Add (other);
+	}
+	public void recieveDrawOnTurn() { //if the player decides to draw
+		handList[handList.Count-1].loadCard (GameObject.Find ("Control").GetComponent<Control> ().playerHand.transform);
+		drew = true;
+		turnEnd (-1);
+	}
+	public void turnEnd(int where)
+	{
+		Control cont = GameObject.Find("Control").GetComponent<Control>();
+		cont.playerHand.GetComponent<RectTransform>().localPosition = new Vector2(0, 0);
+
+		// Clear button listeners and reset hand UI
+		for (int i = cont.playerHand.transform.childCount - 1; i >= 0; i--)
+		{
+			cont.playerHand.transform.GetChild(i).GetComponent<Button>().onClick.RemoveAllListeners();
+			cont.playerHand.transform.GetChild(i).GetChild(3).gameObject.SetActive(false);
+		}
+
+		if (drew)
+		{
+			cont.GetComponent<Control>().enabled = true;
+			cont.recieveText($"{name} drew a card");
+			cont.deckGO.GetComponent<Button>().onClick.RemoveAllListeners();
+			return;
+		}
+
+		Card playedCard = handList[where]; // Save reference before modifying handList
+		int specNumb = playedCard.getNumb();
+
+		if (playedWild)
+		{
+			cont.updateDiscPile(playedCard);
+			handList.RemoveAt(where);
+			cont.startWild(name);
+			if (specNumb == 14)
+				cont.specialCardPlay(this, 14);
+		}
+		else
+		{
+			if (specNumb < 10)
+			{
+				cont.recieveText($"{name} played a {playedCard.getColor()} {playedCard.getNumb()}");
+				cont.enabled = true;
+			}
+			else if (specNumb == 10)
+			{
+				cont.specialCardPlay(this, 10);
+				cont.recieveText($"{name} played a {playedCard.getColor()} skip");
+			}
+			else if (specNumb == 11)
+			{
+				cont.specialCardPlay(this, 11);
+				cont.recieveText($"{name} played a {playedCard.getColor()} reverse");
+			}
+			else if (specNumb == 12)
+			{
+				cont.specialCardPlay(this, 12);
+				cont.recieveText($"{name} played a {playedCard.getColor()} draw 2");
+			}
+			cont.updateDiscPile(playedCard);
+			handList.RemoveAt(where);
+		}
+
+		// Play audio based on the saved card details
+		int toneIndex = playedCard.getNumb();
+		string color = playedCard.getColor();
+		if (toneIndex >= 0 && toneIndex < 10)
+		{
+			cont.PlayCardAudio(color, toneIndex);
+		}
+	}
+	public bool Equals(PlayerInterface other) { //equals function based on name
+		return other.getName ().Equals (name);
+	}
+	public string getName() { //returns the name
+		return name;
+	}
+	public int getCardsLeft() { //gets how many cards are left in the hand
+		return handList.Count;
+	}
+}
